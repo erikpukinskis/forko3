988a1e74d392870bd61e6ad606c9e7ad316b9620
Erik Pukinskis <erik@snowedin.net>
2011-01-15 01:54:15 -0700

    Refactored haml handling

--- forko3.js
+++ forko3.js
@@ -1,9 +1,12 @@
 require.paths.unshift('../vendor/node-router/lib', '../vendor/haml-js/lib');
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-var Router = require('node-router');
-var Haml = require('haml');
+var http = require('http'),
+  fs = require('fs'),
+  path = require('path'),
+  url = require('url'),
+  sys = require('sys'),
+  querystring = require('querystring'),
+  Router = require('node-router'),
+  Haml = require('haml');
 
 var hostname = 'http://localhost:8124'
 
@@ -14,6 +17,12 @@ var App = function (name) {
   this.path = function() {
     return path.join(this.root, this.name.substr(0,1), this.name.substr(0,2), this.name + ".html"); 
   }
+}
+
+http.ServerResponse.prototype.haml = function(view, locals) {
+  haml = fs.readFileSync('views/' + view + '.haml', 'utf8');
+  this.writeHead(200, {'Content-Type': 'text/html'});
+  this.end(Haml.render(haml, {locals: locals})); 
 }
 
 var server = Router.getServer();
@@ -27,11 +36,19 @@ server.get("/", function (request, response) {
 })
 
 server.get(new RegExp("^/fork/([a-z]*)$"), function (request, response, match) {
-  app = new App('start');
+  app = new App(match);
+  app.parent = match
   fs.readFile(app.path(), function (err, data) {
-    haml = fs.readFileSync('views/fork.haml', 'utf8')
+    app.code = data
+    response.haml('fork', {host: hostname, app: app});
+  });
+});
+
+server.post('/apps', function(request, response) {
+  request.addListener("data", function(data) {
+    params = querystring.parse(data);
     response.writeHead(200, {'Content-Type': 'text/html'});
-    response.end(Haml.render(haml, {locals: {app: match, host: hostname, code: data}}));
+    response.end(.parent);
   });
 });
 
