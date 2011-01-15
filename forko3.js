1230c868b3562885b088e16946b3ff7f46d59e4b
Erik Pukinskis <erik@snowedin.net>
2011-01-15 01:03:46 -0700

    Form for forking apps

--- forko3.js
+++ forko3.js
@@ -5,24 +5,37 @@ var path = require('path');
 var Router = require('node-router');
 var Haml = require('haml');
 
-var appPath = '/Users/erik/projects/forko3/apps';
+var hostname = 'http://localhost:8124'
+
+var App = function (name) {
+  this.name = name;
+  this.root = '/Users/erik/projects/forko3/apps';
+  
+  this.path = function() {
+    return path.join(this.root, this.name.substr(0,1), this.name.substr(0,2), this.name + ".html"); 
+  }
+}
 
 var server = Router.getServer();
 
 server.get("/", function (request, response) {
-  var startAppPath = path.join(appPath, "s/st/start.html");
-  fs.readFile(startAppPath, function (err, data) {
+  app = new App('start')
+  fs.readFile(app.path(), function (err, data) {
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.end(data);
   });
 })
 
 server.get(new RegExp("^/fork/([a-z]*)$"), function (request, response, match) {
-  haml = fs.readFileSync('views/fork.haml', 'utf8')
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.end(Haml.render(haml));
+  app = new App('start');
+  fs.readFile(app.path(), function (err, data) {
+    haml = fs.readFileSync('views/fork.haml', 'utf8')
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end(Haml.render(haml, {locals: {app: match, host: hostname, code: data}}));
+  });
 });
 
 server.listen(8124);
 
 console.log('Server running at http://127.0.0.1:8124/');
+
