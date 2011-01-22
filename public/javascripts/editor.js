72eda6238aceffbb755e50d8d47aa2316a6e7e37
Erik Pukinskis <erik@snowedin.net>
2011-01-22 02:12:43 -0700

    Syntax highlighting\!

---
+++ public/javascripts/editor.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	editAreaLoader.init({
+		id: "code"	// id of the textarea to transform		
+		,start_highlight: true	// if start with highlight
+		,allow_resize: "no"
+    ,toolbar: ""
+		,allow_toggle: false
+		,language: "en"
+		,syntax: "html"	
+    ,replace_tab_by_spaces: 2
+	});
+});
